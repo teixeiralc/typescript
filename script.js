b4f80478da1d6c951a6e48b9f7988b7014f6490d
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-04-12 14:34:05 -0300

    Type Predicate exercise

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchData() {
+    const res = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await res.json();
+    handleData(json);
+}
+fetchData();
+function isCourse(value) {
+    if (value && typeof value === 'object' && 'nome' in value && 'horas' in value && 'tags' in value) {
+        return true;
+    }
+    return false;
+}
+function handleData(data) {
+    if (Array.isArray(data)) {
+        data.filter(isCourse).forEach((course) => {
+            document.body.innerHTML += `
+      <div>
+        <h1>${course.nome}</h1>
+        <p>${course.horas}</p>
+        <p>${course.tags}</p>
+      </div>
+      `;
+        });
+    }
+}
