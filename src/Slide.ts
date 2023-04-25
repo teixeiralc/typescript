2b1c0c403fef857ba280e21396384ad9bf86ec73
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-04-25 10:14:00 -0300

    slide: hide and show functions

---
+++ src/Slide.ts
@@ -0,0 +1,28 @@
+export default class Slide {
+  container;
+  slides;
+  controls;
+  time;
+  index: number;
+  slide: Element;
+
+  constructor(container: Element, slides: Element[], controls: Element, time: number = 5000) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.time = time;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.show(this.index);
+  }
+  hide(el: Element) {
+    el.classList.remove('active');
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[index];
+    this.slides.forEach((el) => this.hide(el));
+    this.slides[index].classList.add('active');
+  }
+}
---
+++ src/script.ts
@@ -0,0 +1,9 @@
+import Slide from './Slide.js';
+
+const slideContainer = document.getElementById('slide');
+const slideElements = document.getElementById('slide_elements');
+const slideControls = document.getElementById('slide_controls');
+
+if (slideContainer && slideElements && slideElements.children.length && slideControls) {
+  new Slide(slideContainer, [...slideElements.children], slideControls, 3000);
+}
