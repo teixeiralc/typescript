bf46b77f05dfba9039a39f6a7afb252283573f8b
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-04-25 10:23:53 -0300

    slide: prev and next buttons

--- dist/Slide.js
+++ dist/Slide.js
@@ -12,7 +12,7 @@ export default class Slide {
         this.time = time;
         this.index = 0;
         this.slide = this.slides[this.index];
-        this.show(this.index);
+        this.init();
     }
     hide(el) {
         el.classList.remove('active');
@@ -22,6 +22,28 @@ export default class Slide {
         this.slide = this.slides[index];
         this.slides.forEach((el) => this.hide(el));
         this.slides[index].classList.add('active');
+    }
+    prev() {
+        const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    next() {
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    addControls() {
+        const prevBtn = document.createElement('button');
+        const nextBtn = document.createElement('button');
+        prevBtn.innerText = 'Previous Slide';
+        nextBtn.innerText = 'Next Slide';
+        this.controls.appendChild(prevBtn);
+        this.controls.appendChild(nextBtn);
+        prevBtn.addEventListener('pointerup', () => this.prev());
+        nextBtn.addEventListener('pointerup', () => this.next());
+    }
+    init() {
+        this.addControls();
+        this.show(this.index);
     }
 }
 //# sourceMappingURL=Slide.js.map
--- src/Slide.ts
+++ src/Slide.ts
@@ -14,7 +14,7 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
   hide(el: Element) {
     el.classList.remove('active');
@@ -24,5 +24,28 @@ export default class Slide {
     this.slide = this.slides[index];
     this.slides.forEach((el) => this.hide(el));
     this.slides[index].classList.add('active');
+  }
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+  private addControls() {
+    const prevBtn = document.createElement('button');
+    const nextBtn = document.createElement('button');
+    prevBtn.innerText = 'Previous Slide';
+    nextBtn.innerText = 'Next Slide';
+    this.controls.appendChild(prevBtn);
+    this.controls.appendChild(nextBtn);
+
+    prevBtn.addEventListener('pointerup', () => this.prev());
+    nextBtn.addEventListener('pointerup', () => this.next());
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
